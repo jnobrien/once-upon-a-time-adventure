4e4bc364529655054ffde90214b2197affd64238
Jessica O'Brien <146675559+jnobrien@users.noreply.github.com>
2023-10-25 21:59:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+function enterForest() {
+  var text = document.getElementById("enterForest");
+  text.style.display = "block";
+  var text =document.getElementById("enterForestButton");
+  text.style.display = "none";
+  var text =document.getElementById("aroundForestButton");
+  text.style.display = "none";
+  //var text =document.getElementById("intro");
+  //text.style.display = "none";
+  var text =document.getElementById("restart");
+  text.style.display = "block";
+}
+
+function chooseTree() {
+  var text = document.getElementById("chooseTree");
+  text.style.display = "block";
+  var text =document.getElementById("chooseCloudButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseFireButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseTreeButton");
+  text.style.display = "none";
+  //var text =document.getElementById("enterForestStory");
+  //text.style.display = "none";
+}
+
+function chooseCloud() {
+  var text = document.getElementById("chooseTree");
+  text.style.display = "block";
+  var text =document.getElementById("chooseCloudButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseFireButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseTreeButton");
+  text.style.display = "none";
+  //var text =document.getElementById("enterForestStory");
+  //text.style.display = "none";
+}
+
+function chooseFire() {
+  var text = document.getElementById("chooseFire");
+  text.style.display = "block";
+  var text =document.getElementById("chooseCloudButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseFireButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseTreeButton");
+  text.style.display = "none";
+  //var text =document.getElementById("enterForestStory");
+  //text.style.display = "none";
+}
+
+function chooseRebirth() {
+  var text = document.getElementById("chooseRebirth");
+  text.style.display = "block";
+  var text =document.getElementById("chooseRebirthButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseBalanceButton");
+  text.style.display = "none";
+  //var text =document.getElementById("chooseFireStory");
+  //text.style.display = "none";
+}
+
+function chooseBalance() {
+  var text = document.getElementById("chooseBalance");
+  text.style.display = "block";
+  var text =document.getElementById("chooseRebirthButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseBalanceButton");
+  text.style.display = "none";
+  //var text =document.getElementById("chooseFireStory");
+  //text.style.display = "none";
+}
+
+function aroundForest() {
+  var text = document.getElementById("aroundForest");
+  text.style.display = "block";
+  var text =document.getElementById("enterForestButton");
+  text.style.display = "none";
+  var text =document.getElementById("aroundForestButton");
+  text.style.display = "none";
+  //var text =document.getElementById("intro");
+  //text.style.display = "none";
+  //var text =document.getElementById("restart");
+  //text.style.display = "block";
+}
+
+function chooseAurelia() {
+  var text = document.getElementById("chooseAurelia");
+  text.style.display = "block";
+  var text =document.getElementById("chooseAureliaButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseValeriaButton");
+  text.style.display = "none";
+  //var text =document.getElementById("aroundForestStory");
+  //text.style.display = "none";
+}
+
+function chooseGemstones() {
+  var text = document.getElementById("chooseGemstones");
+  text.style.display = "block";
+  var text =document.getElementById("chooseGemstonesButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseKnowledgeButton");
+  text.style.display = "none";
+  //var text =document.getElementById("chooseAureliaStory");
+  //text.style.display = "none";
+}
+
+function chooseKnowledge() {
+  var text = document.getElementById("chooseKnowledge");
+  text.style.display = "block";
+  var text =document.getElementById("chooseGemstonesButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseKnowledgeButton");
+  text.style.display = "none";
+  //var text =document.getElementById("chooseAureliaStory");
+  //text.style.display = "none";
+}
+
+function chooseValeria() {
+  var text = document.getElementById("chooseValeria");
+  text.style.display = "block";
+  var text =document.getElementById("chooseAureliaButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseValeriaButton");
+  text.style.display = "none";
+  //var text =document.getElementById("aroundForestStory");
+  //text.style.display = "none";
+}
+
+function chooseSubmit() {
+  var text = document.getElementById("chooseSubmit");
+  text.style.display = "block";
+  var text =document.getElementById("chooseSubmitButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseWarButton");
+  text.style.display = "none";
+  //var text =document.getElementById("chooseValeriaStory");
+  //text.style.display = "none";
+}
+
+function chooseWar() {
+  var text = document.getElementById("chooseWar");
+  text.style.display = "block";
+  var text =document.getElementById("chooseSubmitButton");
+  text.style.display = "none";
+  var text =document.getElementById("chooseWarButton");
+  text.style.display = "none";
+  //var text =document.getElementById("chooseValeriaStory");
+  //text.style.display = "none";
+}
+
